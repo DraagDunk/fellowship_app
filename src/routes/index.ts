e242cba83291f76de756b6cd8177629542356a8a
Jesper <jesper.dk@hotmail.dk>
2026-05-08 19:47:20 +0200

    Initialize project, rudimentary Dockerfile

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response, NextFunction } from 'express';
-const router: Router = Router();
-
-/* GET home page. */
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.render('index.ejs', { title: 'The Fantasy Fellowship' });
-});
-
-export default router;
